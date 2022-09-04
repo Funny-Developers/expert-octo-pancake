e80c4ab7f554432ed691b492702c2442c5941e9e
crazy_piggy_ <57442208+crazy-piggy@users.noreply.github.com>
2022-09-04 13:32:22 +0800

    Start program

---
+++ src/main/java/org/funnydevelopers/expertoctopancake/Main.java
@@ -0,0 +1,16 @@
+package org.funnydevelopers.expertoctopancake;
+
+public class Main {
+	public static void main(String[] args) {
+        System.out.println("Hello, welcome to use our lib!");
+        System.out.println("--------------------------------------------------");
+        System.out.println("|How to use:                                     |");
+        System.out.println("|You can run this file in command line and it    |");
+        System.out.println("|will show this message.                         |");
+        System.out.println("|You can run this file with Fabric loader and    |");
+        System.out.println("|Fabric API in Minecraft 1.19.x.                 |");
+        System.out.println("|You can use it as an external library to develop|");
+        System.out.println("|your own Fabric mods.                           |");
+        System.out.println("--------------------------------------------------");
+    }
+}
